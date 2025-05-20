3cb1730e437c627629330a8755567af153d4daab
Numstr <Numstr@users.noreply.github.com>
2025-05-20 23:34:01 +0300

    refactor: change google.com to cern.ch

--- Updater.bat
+++ Updater.bat
@@ -27,7 +27,7 @@ if %USE_PROXY% == true (
 
 :::::: NETWORK CHECK
 
-%CURL% -I -s %CURL_PROXY% https://www.google.com | %BUSYBOX% grep -q "HTTP"
+%CURL% -I -s %CURL_PROXY% https://cern.ch | %BUSYBOX% grep -q "HTTP"
 
 if "%ERRORLEVEL%" == "1" (
   echo Check Your Network Connection
